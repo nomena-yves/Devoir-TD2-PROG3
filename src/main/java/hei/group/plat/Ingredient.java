c2e7b0c3cfde22ed82ceeb32b6d27f9bd025e342
nomena <hei.nomenjanahary@gmail.com>
2025-12-18 21:59:45 +0300

    formation des getter setter et constructeur

--- src/main/java/hei/group/plat/Ingredient.java
+++ src/main/java/hei/group/plat/Ingredient.java
@@ -5,4 +5,43 @@ public class Ingredient {
     private String name;
     private Double price;
     private CategoryEnum category;
+
+    public Ingredient(int id, String name, Double price, CategoryEnum category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
 }
