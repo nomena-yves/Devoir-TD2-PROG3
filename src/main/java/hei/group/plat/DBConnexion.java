ba336e1246aa12405b15dc234bcd86d7ef0964e5
nomena <hei.nomenjanahary@gmail.com>
2025-12-29 14:42:01 +0300

    amelioration du code

--- src/main/java/hei/group/plat/DBConnexion.java
+++ src/main/java/hei/group/plat/DBConnexion.java
@@ -16,16 +16,5 @@ public class DBConnexion {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static void main(String[] args) {
-        DBConnexion db = new DBConnexion();
-        try (Connection conn = db.getConnection()) {
-            if (conn != null) {
-                System.out.println("Connexion réussie à la base de données !");
-            } else {
-                System.out.println("Échec de la connexion !");
-            }
-        } catch (SQLException e) {
-            System.out.println("Erreur de connexion : " + e.getMessage());
-        }
-    }
+
 }
